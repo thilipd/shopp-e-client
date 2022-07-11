0920b65d583d2e053c48abb7ca9d0640b7c3f8c9
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-07-11 23:07:34 +0530

    Add existing file

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://shopp-e-server.herokuapp.com/',
     responseType: 'json',
     timeout: 10000,
 
