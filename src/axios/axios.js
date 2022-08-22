e3ec9fe885b25f3b319be5bdc0db128621e28e04
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-08-23 00:56:01 +0530

    changes

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,15 +1,17 @@
 
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
-axios.defaults.crossorigin = false;
+// axios.defaults.withCredentials = true;
+// axios.defaults.crossorigin = false;
 
 const instance = axios.create({
-    baseURL: 'https://shopp-e-server.herokuapp.com',
+    baseURL: 'https://shopp-e-server.herokuapp.com/',
+    // baseURL: ' http://localhost:8000',
     responseType: 'json',
     timeout: 10000,
 
     headers: {
+        'Access-Control-Allow-Origin': 'https://shopp-e-server.herokuapp.com/',
         'X-Requested-With': 'XMLHttpRequest',
         'Content-Type': 'application/json'
     }
